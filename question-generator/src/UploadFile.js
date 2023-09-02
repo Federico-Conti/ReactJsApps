a0566b98ad77d2bd58e317ba3c0bcb294c47c2f0
brutalfede <federicoconti1702@gmail.com>
2023-09-02 13:06:15 +0200

    questionGenerator|check file upload extension

--- question-generator/src/UploadFile.js
+++ question-generator/src/UploadFile.js
@@ -5,7 +5,8 @@ import { Context } from './context'
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import Tooltip from '@mui/material/Tooltip';
-
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 //theme factory set in likeColors
 import { ThemeProvider } from '@mui/material/styles';
@@ -14,11 +15,21 @@ import { theme } from './likeColors';
 function UploadFile() {
 
     const [questionsList, SetQuestionsList, questionsToBeExtracted, SetQuestionsToBeExtracted, extractedQuestions, SetExtractedQuestions] = useContext(Context);
+    const [error, setError] = useState(false);
 
     let fileReader;
+    const validTextExtensions = ['txt', 'csv', 'json', 'rtf', 'xml', 'md', 'html', 'markdown', 'plist'];
+
+    function isTextFile(file) {
+        let filename = file.name;
+        let fileExtension = filename.split('.').pop().toLowerCase();
+        return validTextExtensions.includes(fileExtension);
+    }
 
     const onChange = e => {
         let file = e.target.files;
+        if (!isTextFile(file[0])) { setError(true); return; };
+
         fileReader = new FileReader();
         fileReader.onloadend = handleFileRead;
         fileReader.readAsText(file[0]);
@@ -27,7 +38,7 @@ function UploadFile() {
     const checkEmptyLines = array => {
         for (let index = 0; index < array.length; index++) {
             if (array[index] == "")
-            array.splice(index, 1);
+                array.splice(index, 1);
         }
         return array;
     };
@@ -36,20 +47,24 @@ function UploadFile() {
     const handleFileRead = e => {
         let content = fileReader.result;
         content = content.trim();
-        let arr = content.split('\r\n').filter((str) => str !== "");
+        let arr = content.split('\n').filter((str) => str !== "");
         SetQuestionsList([...arr]);
         SetQuestionsToBeExtracted([...arr]);
 
     };
 
-    const message = "questions must be separated by a line item"
+
+    const message = "questions must be separated by a carriage return or a blank line ";
+
     return (
         <div className="uplodaile__container">
+
             <Tooltip title={message}>
+
                 <label htmlFor="upload-questions">
-                    <input style={{ display: "none" }} id="upload-questions" type="file" name="upload-questions" onChange={onChange} />
+                    <input style={{ display: "none" }} id="upload-questions" type="file" accept=".txt, .csv, .json, .rtf, .html, .md, .markdown, .plist, .csv, .xml" name="upload-questions" onChange={onChange} />
                     <Fab
-                      style={{ backgroundColor: theme.status.uploadFile, color:"white" }}
+                        style={{ backgroundColor: theme.status.uploadFile, color: "white" }}
                         size="medium"
                         component="span"
                         aria-label="add"
@@ -62,6 +77,13 @@ function UploadFile() {
                     </Fab>
                 </label>
             </Tooltip>
+            {error && (
+                <Stack sx={{ width: '100%' }} spacing={2}>
+                    <Alert severity="error" style={{ margin: '15px', borderRadius: '15px', alignContent: 'center' }}>
+                        file extension not permitted
+                    </Alert>
+                </Stack>
+            )}
         </div>
     )
 }
